adff170ab37aac4cf7a4d569e8bf54ea969bd914
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-04-10 12:10:28 -0700

    Display the computed total price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -61,6 +61,7 @@ class BurgerBuilder extends Component {
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
+                    price={this.state.totalPrice}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo} />
