4da48729b49df841ae969f0bcfa5f944df1ccec4
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-04-11 10:17:11 -0700

    Add build folder

---
+++ build/precache-manifest.da0bb093e3e3eb387825ff44b16ed3af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4098705f1aee76abb491",
+    "url": "/static/js/main.ac2609f1.chunk.js"
+  },
+  {
+    "revision": "46845d6085ae1cee3cba",
+    "url": "/static/js/2.c653bda5.chunk.js"
+  },
+  {
+    "revision": "4098705f1aee76abb491",
+    "url": "/static/css/main.9fba0579.chunk.css"
+  },
+  {
+    "revision": "9aaf1ec47b5e27603fb95ee25eb45b8f",
+    "url": "/index.html"
+  }
+];
